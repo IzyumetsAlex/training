d7b5e956e5446b8f9ec1e940e8767bc76b382210
a.izyumets <izumets.alex@mail.ru>
2025-08-22 18:21:36 +0300

    arr

---
+++ src/main/java/ru/education/CatList.java
@@ -0,0 +1,33 @@
+package ru.education;
+
+import java.util.ArrayList;
+
+public class CatList {
+    private final String name;
+
+    public CatList(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<CatList> cats = new ArrayList<>();
+
+        CatList thomas = new CatList("Томас");
+        CatList behemoth = new CatList("Бегемот");
+        CatList philipp = new CatList("Филипп Маркович");
+        CatList pushok = new CatList("Пушок");
+
+        cats.add(thomas);
+        cats.add(behemoth);
+        cats.add(philipp);
+        cats.add(pushok);
+
+            for (CatList cat : cats) {
+                if (cat.name.equals("Бегемот")) {
+                    cats.remove(cat);
+                }
+            }
+            System.out.println(cats);
+        }
+    }
+}
