9268b7a8f79e0b3e82e21927fff6483f8da959e6
a.izyumets <izumets.alex@mail.ru>
2025-08-22 14:58:26 +0300

    arr

--- src/main/java/ru/education/ArraysList.java
+++ src/main/java/ru/education/ArraysList.java
@@ -13,19 +13,42 @@ public class ArraysList {
     }
 
     public static <T> void removeElement(List<T> list, T element) {
-
+        for (T listItem : list) {
+            if (listItem.equals(element)) {
+                list.remove(listItem);
+            }
+        }
     }
 
-    static class Car {
-        private final String brand;
-        private final int yearOfProduction;
+        static class Car {
+            private final String brand;
+            private final int yearOfProduction;
 
-        public Car(String brand, int yearOfProduction) {
-            this.brand = brand;
-            this.yearOfProduction = yearOfProduction;
+            public Car(String brand, int yearOfProduction) {
+                this.brand = brand;
+                this.yearOfProduction = yearOfProduction;
+            }
+
+            @Override
+            public String toString() {
+                return "[brand=" + brand + ", year=" + yearOfProduction + "]";
+            }
+
+            @Override
+            public int hashCode() {
+                return Objects.hash(brand, yearOfProduction);
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                if(this==o) return true;
+                if(getClass() != o.getClass()) return false;
+                Car car = (Car) o;
+                return brand.equals(car.brand) && yearOfProduction ==car.yearOfProduction;
+            }
         }
 
-    }
+
 
     public static void main(String[] args) {
         List<Car> cars = Arrays.asList(
@@ -42,6 +65,7 @@ public class ArraysList {
         int index = findIndex(uniqueCars, new Car("Audi", 2017));
         System.out.println("Индекс Audi 2017: " + index);
 
-
+        removeElement(uniqueCars, new Car("Audi", 2017));
+        System.out.println("Список:" + uniqueCars);
     }
 }
